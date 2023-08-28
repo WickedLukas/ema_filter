1ae4b452cd5575e403f1f2f6e9adadfa68cbdeb1
Lukas <lukas_m@gmx.de>
2023-08-28 12:07:14 +0200

    Switch to template function in order to enable higher precision.

--- ema_filter.h
+++ ema_filter.h
@@ -9,6 +9,9 @@
 #ifndef __EMA_FILTER_H__
 #define __EMA_FILTER_H__
 
-float ema_filter(float current_value, float previous_value, float ema_alpha);
+template<typename V, typename F>
+F ema_filter(V current_value, F previous_value, float ema_alpha) {
+	return ((F) ema_alpha * current_value + (1 - ema_alpha) * previous_value);
+}
 
 #endif //__EMA_FILTER_H__
